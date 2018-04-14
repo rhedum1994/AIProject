618f39427da9b591497a061906ef1a28992a42c6
Jill Pulicicchio <jillianpulicicchio@gmail.com>
2018-04-14 17:01:29 -0400

    Create guessWho.py

---
+++ guessWho.py
@@ -0,0 +1,22 @@
+print("Hello and welcome to the marvelous game of Guess Who!")
+print("Pick a character and I'll ask you a series of questions and try to guess who you picked.")
+print("Do you wish to continue? y or n?")
+value = raw_input()
+if value == "y":
+    print("Lets play! Enter 'q' to quit")
+else:
+    print("aight. bye")
+#while True:
+    # Get input.
+#    value = raw_input()
+
+    # Break if user types q.
+#    if value == "q":
+#        break
+
+    # Display value.
+#    print("You typed: ", value)
+
+# Exit message.
+#print("You quit.")
+
