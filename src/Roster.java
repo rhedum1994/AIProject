eb70f470bf57fbde988cb3c3681ddf6bb896883e
curtisbaillie <bailliec7@students.rowan.edu>
2018-04-17 14:26:16 -0400

    src folder

---
+++ src/Roster.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+
+public class Roster {
+
+    private ArrayList<Character> charList;
+
+
+
+    public Roster() {
+        charList = new ArrayList<>();
+    }
+
+
+
+    public void addChar(Character ch){
+        charList.add(ch);
+    }
+
+    public void removeChar(Character ch){
+        charList.remove(ch);
+    }
+
+
+    public int getSize() {
+        return charList.size();
+    }
+
+    //get attributes of each playable character
+    public String getAttributes() {
+
+        String attributes = "";
+
+        for(int i = 0; i < charList.size(); i++) {
+            attributes += charList.get(i).getName() + " has attributes: " + "eye color " + charList.get(i).getEye() + " hair color: " + charList.get(i).getHair() + " shirt color: " + charList.get(i).getShirt() + " gender: " + charList.get(i).getGender() + "\n";
+
+        }
+        return attributes;
+    }
+    //get attributes of Player's character
+    public String getMyCharacter() {
+        String myChar = "";
+        for(int i = 0; i< charList.size(); i++) {
+
+            if(charList.get(i).isMyCharacter()){
+                myChar += charList.get(i).getName() + " has attributes: " + "eye color " + charList.get(i).getEye() + " hair color: " + charList.get(i).getHair() + " shirt color: " + charList.get(i).getShirt() + " gender: " + charList.get(i).getGender() + "\n";
+            }
+        }
+        return myChar;
+    }
+
+
+
+    public void removeCharacterHair(String charhair) {
+        for(int i = 0; i < charList.size(); i++ ){
+            if(!charList.get(i).isHair(charhair)){
+               charList.remove(i);
+            }
+        }
+    }
+
+    public void removeCharacterGender(String chargen) {
+        for(int i = 0; i < charList.size(); i++ ){
+            if(!charList.get(i).isGender(chargen)){
+                charList.remove(i);
+            }
+        }
+    }
+
+    public void removeCharacterShirt(String charshirt) {
+        for(int i = 0; i < charList.size(); i++ ){
+            if(!charList.get(i).isShirt(charshirt)){
+                charList.remove(i);
+            }
+        }
+    }
+
+    public void removeCharacterEye(String chareye) {
+        for(int i = 0; i < charList.size(); i++ ){
+            if(!charList.get(i).isEye(chareye)){
+                charList.remove(i);
+            }
+        }
+    }
+
+    public String toString()
+    {
+        String showItems="";
+
+        if(charList.size() == 1){
+            showItems = "The character is " + charList.get(0).toString();
+        }
+        else
+        {
+            showItems = "The characters are: ";
+        for(int i = 0; i<charList.size(); i++) {
+            if (charList.size() - 1 == i) {
+                showItems += "and " + charList.get(i).toString();
+            } else {
+                showItems += charList.get(i).toString() + ", ";
+            }
+        }
+        }
+
+        return  showItems;
+    }
+
+
+
+}
---
+++ src/driver.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.io.*;
+
+public class driver {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    public static void main(String args[]) throws IOException {
+        //Gameboard setup
+        //Make characters
+        Character Jill = new Character("red", "brown:", "female", "green", "Jill");
+        Character Sean = new Character("red", "green", "male", "green", "Sean");
+        Character Becky = new Character("brown", "brown", "female", "blue", "Becky");
+        Character Tom = new Character("brown", "green", "male", "blue", "Tom");
+        Character Caroline = new Character("blonde", "blue", "female", "blue", "Caroline");
+        Character Curtis = new Character("brown", "blue", "male", "red", "Curtis");
+        Character Steph = new Character("brown", "brown", "female", "red", "Steph");
+
+        //Add characters to ArrayList
+        Roster people = new Roster();
+        people.addChar(Jill);
+        people.addChar(Sean);
+        people.addChar(Becky);
+        people.addChar(Tom);
+        people.addChar(Caroline);
+        people.addChar(Curtis);
+        people.addChar(Steph);
+
+        //create gameplay loop
+        boolean noWinner = true;
+
+        //create turn loop
+        boolean turn = true;
+
+
+        //Create Menu and character names
+        System.out.println("Welcome to Guess Who!");
+        System.out.println("You will be assigned a random character from the list of created characters");
+        System.out.println("You can either guess who the computer has, or ask a question");
+        System.out.println("Once you have either made a guess or asked a question, you turn ends and the computer will go");
+        System.out.println("Your goal is to guess who the computer has before it guesses your character first!");
+        System.out.println("Good Luck!");
+        while(noWinner) {
+            printMenu();
+            while(turn) {
+
+
+                String s = in.readLine().trim();
+                int i = Integer.parseInt(s);
+                System.out.print(i);
+                System.out.println("");
+
+                switch (i) {
+            //Ask question
+                    case 1:
+
+                        break;
+            //Guess computer's character
+                    case 2:
+
+                        break;
+            //See list of playable characters
+                    case 3:
+
+                        break;
+            //see ALL characters' attributes
+                    case 4:
+
+                        break;
+            //See User's character and their attributes
+                    case 5:
+
+                        break;
+            //Quit game
+                    case 6:
+
+                        break;
+
+                }
+            }
+        }
+    }
+
+    public static void printMenu(){
+        System.out.println("Your game options are:");
+        System.out.println("1. Ask Question (ends turn)");
+        System.out.println("2. Guess Computer's character");
+        System.out.println("3. See List of playable characters in game");
+        System.out.println("4. See ALL Characters' attributes");
+        System.out.println("5. See MY character and their attributes");
+        System.out.println("6. Quit Game");
+
+    }
+
+
+    //option 3
+    // returns toString of all playable characters
+    public static String displayCharacters(Roster temp){
+        return temp.toString();
+    }
+
+    //option 4
+    // returns a String concactinated of all attributes
+    //use system.out.print when calling method
+    public static String displayAttributes(Roster temp) {
+
+        return temp.getAttributes();
+    }
+
+    //option 5
+    //returns a String of Player's character name with attributes
+    //use system.out.print when calling method
+    public static String displayMyAtttributes(Roster temp) {
+        return temp.getMyCharacter();
+    }
+
+
+
+
+}
